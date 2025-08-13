7d8ec594603920fcea6362ebfc261ab76bd5ba70
omarcarp2314-ship-it <omarcarp2314@gmail.com>
2025-08-13 17:35:25 -0500

    Create ejercicios.js
    feat: add ejercicio1 - objeto rutina.

---
+++ week0/ejercicios.js
@@ -0,0 +1,12 @@
+let nombreRutina = "Salto explosivo";
+let duracionMinutos = 30;
+let nivelDificultad = "Intermedio";
+
+let rutina = {
+  nombreRutina,
+  duracionMinutos,
+  nivelDificultad
+};
+
+console.log("Rutina creada:");
+console.table(rutina);
